3f0e2deff82434c47a0dbc49501d32ba4b564b24
joan30122001 <ngounouloic675@gmail.com>
2022-08-24 17:43:10 +0100

    allow the API

--- taniere/settings.py
+++ taniere/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t6%+_g^fr*me07d6l+^f21^97j+z1m6ktl%sk=&dy!o=iesxz_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
